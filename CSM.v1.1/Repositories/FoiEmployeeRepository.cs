fefad80fab2ecd3baa40821a47afb9562d85ca98
Antonijo Hamzić <ahamzic20@foi.hr>
2022-05-28 17:20:53 +0200

    Dodan FoiEmployee repozitorij

---
+++ CSM.v1.1/Repositories/FoiEmployeeRepository.cs
@@ -0,0 +1,74 @@
+﻿using CSM.v1._1.Class;
+using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSM.v1._1.Repositories
+{
+    public class FoiEmployeeRepository
+    {
+        public static FoiEmployee GetFoiEmployee(int id)
+        {
+            FoiEmployee foiemployee = null;
+
+            string sql = $"SELECT * FROM FoiEmployees WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                foiemployee = CreateObject(reader);
+                reader.Close();
+            }
+
+            DB.CloseConnection();
+            return foiemployee;
+        }
+
+        public static List<FoiEmployee> GetFoiEmployees()
+        {
+            List<FoiEmployee> foiemployees = new List<FoiEmployee>();
+
+
+
+            string sql = "SELECT * FROM FoiEmployees";
+
+            DB.SetConfiguration("ahamzic20_DB", "ahamzic20", "pNzDysZy");
+
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                FoiEmployee foiemployee = CreateObject(reader);
+                foiemployees.Add(foiemployee);
+            }
+
+            reader.Close();
+            DB.CloseConnection();
+
+            return foiemployees;
+        }
+
+        private static FoiEmployee CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+
+
+            var foiemployee = new FoiEmployee
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+
+            };
+
+            return foiemployee;
+        }
+    }
+}
