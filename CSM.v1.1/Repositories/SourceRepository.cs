1b5e3f934b53f3242081828055c4800ac1553f11
Antonijo Hamzić <ahamzic20@foi.hr>
2022-05-28 17:31:12 +0200

    Dodan Source repozitorij

---
+++ CSM.v1.1/Repositories/SourceRepository.cs
@@ -0,0 +1,74 @@
+﻿using CSM.v1._1.Class;
+using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSM.v1._1.Repositories
+{
+    public class SourceRepository
+    {
+
+        public static Source GetSource(int id)
+        {
+            Source source = null;
+
+            string sql = $"SELECT * FROM Source WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                source = CreateObject(reader);
+                reader.Close();
+            }
+
+            DB.CloseConnection();
+            return source;
+        }
+
+        public static List<Source> GetEquipmentTypes()
+        {
+            List<Source> sources = new List<Source>();
+
+
+
+            string sql = "SELECT * FROM Source";
+
+            DB.SetConfiguration("ahamzic20_DB", "ahamzic20", "pNzDysZy");
+
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Source source= CreateObject(reader);
+                sources.Add(source);
+            }
+
+            reader.Close();
+            DB.CloseConnection();
+
+            return sources;
+        }
+
+        private static Source CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string Name = reader["Name"].ToString();
+
+
+
+            var source = new Source
+            {
+                Id = id,
+                Name = Name,
+            };
+
+            return source;
+        }
+
+    }
+}
