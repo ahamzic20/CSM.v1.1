3a11167ae1c57949f1ff61ba19f0ec5315c7c347
Antonijo Hamzić <ahamzic20@foi.hr>
2022-05-29 19:32:28 +0200

    Uređena forma za prikaz opreme

--- CSM.v1.1/FrmEquipment.cs
+++ CSM.v1.1/FrmEquipment.cs
@@ -40,7 +40,15 @@ namespace CSM.v1._1
            
 
             dgvEquipment.Columns["Id"].DisplayIndex = 0;
-            
+            dgvEquipment.Columns["EquipmentName"].DisplayIndex = 1;
+            dgvEquipment.Columns["ProjectName"].DisplayIndex = 2;
+            dgvEquipment.Columns["Description"].DisplayIndex = 3;
+            dgvEquipment.Columns["EquipmentType"].DisplayIndex = 4;
+            dgvEquipment.Columns["SourceName"].DisplayIndex = 5;
+            dgvEquipment.Columns["DateTime"].DisplayIndex = 6;
+            dgvEquipment.Columns["SupplierName"].DisplayIndex = 7;
+            dgvEquipment.Columns["EmployeeName"].DisplayIndex = 8;
+
 
         }
 
